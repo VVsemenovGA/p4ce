4ca5b294d783a0aa44296e0cf15e14c3eadae567
vv.semenov <revas@list.ru>
2025-06-23 16:07:48 +0300

    + Lab1.5

---
+++ Lab1.5/configfiles.py
@@ -0,0 +1,15 @@
+import glob
+
+ips = set()
+
+for x in glob.glob("C://config_files//*.log"):
+    with open(x) as file:
+        for x in file:
+            if x.find("ip address") == 0 or x.find("ip address") == 1:
+                x = x.replace("ip address", "").strip()
+                x = x.replace("sub", "").strip()
+                ips.add(x)
+num = 1
+for i in ips:
+    print(num, ": ", i)
+    num += 1
