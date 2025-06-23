6009fbef44ff808385c4f04fe149d73ac9df8ed1
vv.semenov <revas@list.ru>
2025-06-23 16:41:12 +0300

    + Lab1.5

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -1,8 +1,9 @@
 import glob
 
 ips = set()
+files_name = glob.glob("C://config_files//*.log")
 
-for x in glob.glob("C://config_files//*.log"):
+for x in files_name:
     with open(x) as file:
         for x in file:
             if x.find("ip address") == 0 or x.find("ip address") == 1:
@@ -10,6 +11,6 @@ for x in glob.glob("C://config_files//*.log"):
                 x = x.replace("sub", "").strip()
                 ips.add(x)
 num = 1
-for i in ips:
-    print(num, ": ", i)
+for i in sorted(ips):
+    print(num, ":", i)
     num += 1
